ddd3a8a9a3f5506e25f47477f2ddb5ff3b252431
Domingo Rosa <DomingoRosa@Domingos-MacBook-Air.local>
2015-10-14 16:46:51 -0700

    cleaned up main.js

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-$(document).ready(function(){
-	$("#addBtn").on("click", function(){
-		var num_one = $("#num_one").val();
-		var num_two = $("#num_two").val();
-		$("#answer").html(parseInt(num_one, 10) + parseInt(num_two, 10));
+$(document).ready(function(){/* opening document.ready*/
+	$("#addBtn").on("click", function(){/*Click Event begins here*/
+		var val1 = $("#num_one").val();
+		var val2 = $("#num_two").val();
+		$("#answer").html(parseInt(val1, 10) + parseInt(val2, 10));
 	});
 	
 	$("#subtractBtn").on("click", function(){
-		var num_one = $("#num_one").val();
-		var num_two = $("#num_two").val();
-		$("#answer").html(parseInt(num_one, 10) - parseInt(num_two, 10));
+		var val1 = $("#num_one").val();
+		var val2 = $("#num_two").val();
+		$("#answer").html(parseInt(val1, 10) - parseInt(val2, 10));
 	});
 	$("#multiplyBtn").on("click", function(){
-		var num_one = $("#num_one").val();
-		var num_two = $("#num_two").val();
-		$("#answer").html(parseInt(num_one, 10) * parseInt(num_two, 10));
+		var val1 = $("#num_one").val();
+		var val2 = $("#num_two").val();
+		$("#answer").html(parseInt(val1, 10) * parseInt(val2, 10));
 	});
 	$("#divideBtn").on("click", function(){
-		var num_one = $("#num_one").val();
-		var num_two = $("#num_two").val();
-		$("#answer").html(parseInt(num_one, 10) / parseInt(num_two, 10));
+		var val1 = $("#num_one").val();
+		var val2 = $("#num_two").val();
+		$("#answer").html(parseInt(val1, 10) / parseInt(val2, 10));
 	});
-});
+});/* closing document.ready*/
