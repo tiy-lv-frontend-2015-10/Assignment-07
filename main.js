e68088eafa0cc9a2bdbc3bade9c800239fa0bcb5
Thomas Francis Shannon <Thomas@Thomas-MacBook-Pro.local>
2015-11-09 14:23:11 -0800

    added all numbers

--- main.js
+++ main.js
@@ -1,51 +1,86 @@
 $(document).ready(function(){
 
 	
-$('#add').on("click",function() {
-	var num_1=$("#num_1").val();
-	var num_2=$("#num_2").val();
-	$("#answer").html(add(num_1,num_2));
+	$('#add').on("click",function() {
+		
+		$("#answer").append("+");
+	});
+	$('#subtract').on("click",function() {
+
+		$("#answer").append("-");
+	});
+	$('#multiply').on("click",function() {
+		
+		$("#answer").append("*")
+	});
+	$('#divide').on("click",function() {
+		
+		$("#answer").append("/");
+	});
+
+	$('#clear').on("click",function() {
+		
+		$("#answer").html("");
+	});
+
+	$('#equal').on("click",function(){
+		
+		sum();
+	});
+	function sum() {
+			var ans = eval($("#answer").html());
+			$("#answer").html(ans)
+		};
+		
+	
+	$("#")
+	$("#0").on("click",function(){
+		$("#answer").append(0);
+
+
+	});
+	$("#1").on("click",function(){
+		$("#answer").append(1);
+
+	});
+	$("#2").on("click",function(){
+		$("#answer").append(2);
+
+	});
+	$("#3").on("click",function(){
+		$("#answer").append(3);
+
+	});
+	$("#4").on("click",function(){
+		$("#answer").append(4);
+
+	});
+	$("#5").on("click",function(){
+		$("#answer").append(5);
+
+	});
+	$("#6").on("click",function(){
+		$("#answer").append(6);
+
+	});
+	$("#7").on("click",function(){
+		$("#answer").append(7);
+
+	});
+	$("#8").on("click",function(){
+		$("#answer").append(8);
+
+	});
+	$("#9").on("click",function(){
+		$("#answer").append(9);
+
+	});
+
+
+
+
+
 });
-$('#subtract').on("click",function() {
-	var num_1=$("#num_1").val();
-	var num_2=$("#num_2").val();
-	$("#answer").html(subtract(num_1,num_2));
-});
-$('#multiply').on("click",function() {
-	var num_1=$("#num_1").val();
-	var num_2=$("#num_2").val();
-	$("#answer").html(multiply(num_1,num_2))
-});
-$('#divide').on("click",function() {
-	var num_1=$("#num_1").val();
-	var num_2=$("#num_2").val();
-	$("#answer").html(divide(num_1,num_2));
-});
-
-
-
-
-
-
-
-var add= function(num_1,num_2) {
-	return parseInt(num_1,10) + parseInt(num_2,10);
-}
-var subtract= function(num_1,num_2) {
-	return parseInt(num_1,10) - parseInt(num_2,10);
-}
-var multiply= function(num_1,num_2) {
-	return parseInt(num_1,10) * parseInt(num_2,10);
-}
-var divide= function(num_1,num_2) {
-	return parseInt(num_1,10) / parseInt(num_2,10);
-}
-
-
-});
-
-
-
 
 
 
